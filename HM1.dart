fd4247e75749706a1d8e4cb4ae2e540975b1d867
Huda-Hassouneh <huda.hasouneh@gmail.com>
2026-09-15 16:00:48 +0300

    first commit

---
+++ HM1.dart
@@ -0,0 +1,23 @@
+void main() {
+  double mark1 = 70;
+
+  switch (mark1) {
+    case 90:
+      print('A');
+      break;
+    case 80:
+      print('B');
+      break;
+    case 70:
+      print('C');
+      break;
+    case 60:
+      print('D');
+      break;
+    case 50:
+      print('F');
+      break;
+    default:
+      print('Invalid mark');
+  }
+}
