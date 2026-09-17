b961b60d37536e9b9fc8f67c96e9706c458e73ff
Noor Hassouneh <159267324+noorhassouneh@users.noreply.github.com>
2026-09-17 13:54:26 +0300

    HM1

--- HM1.dart
+++ HM1.dart
@@ -1,23 +1,28 @@
 void main() {
-  double mark1 = 70;
-
-  switch (mark1) {
-    case 90:
-      print('A');
-      break;
-    case 80:
-      print('B');
-      break;
-    case 70:
-      print('C');
-      break;
-    case 60:
-      print('D');
-      break;
-    case 50:
-      print('F');
-      break;
-    default:
-      print('Invalid mark');
-  }
+  List<double> marks = [70, 50, 76.3, 88, 90, 60];
+  int i = 0;
+  
+  do {
+    switch (marks[i]) {
+        
+      case >90:
+        print('A');
+        break;
+      case <90 && >80:
+        print('B');
+        break;
+      case <80 && >70:
+        print('C');
+        break;
+      case <70 && >60:
+        print('D');
+        break;
+      case <60 && >50:
+        print('F');
+        break;
+      default:
+        print('Invalid mark');
+    }
+    i++;
+  } while (i < marks.length);
 }
